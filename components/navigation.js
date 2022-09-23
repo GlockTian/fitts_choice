ede2943f99e0c299a449007abc89d35fd96fc427
Peter Zhong <petertianzhong@gmail.com>
2022-09-23 19:41:26 +1200

    merge dnd and dnd[id]

--- components/navigation.js
+++ components/navigation.js
@@ -8,7 +8,12 @@ import { useRouter } from 'next/router';
 export function Navigation({children}) {
   const router = useRouter()
   const navStyle = (path)=>{
-    return router.pathname === path
+    if (path === "/"){
+      return router.pathname === path
+      ?"mx-2 rounded-none px-0 border-b-blue-400 text-primary bg-paper"
+      :"mx-2 rounded-none px-0 bg-paper text-black"
+    }
+    return router.pathname.includes(path)
     ?"mx-2 rounded-none px-0 border-b-blue-400 text-primary bg-paper"
     :"mx-2 rounded-none px-0 bg-paper text-black"
   }
@@ -18,7 +23,7 @@ export function Navigation({children}) {
       padding="md"
       header={<Header  height={60} p="xs">
         <Link href="/"><Button className={navStyle("/")}>Home</Button></Link>
-        <Link href="/dnd"><Button className={navStyle("/dnd")}>Fitts' Choice</Button></Link>
+        <Link href="/dnd/1"><Button className={navStyle("/dnd")}>Fitts' Choice</Button></Link>
         <Link href="/intro"><Button className={navStyle("/intro")}>Fitts' Law</Button></Link>
       </Header>}
       styles={(theme) => ({
