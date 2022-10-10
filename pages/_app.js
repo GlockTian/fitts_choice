86bb49d31c952fc28dd9ea8de4037d853dce670d
GlockTian <petertianzhong@gmail.com>
2022-10-10 22:26:42 +1300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import { MantineProvider } from "@mantine/core";
 import { Navigation } from "../components/navigation";
 import Head from "next/head";
 import "../styles/globals.css";
@@ -7,18 +6,9 @@ function MyApp({ Component, pageProps }) {
   // create a mantine provider
   return (
     <div>
-
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme: "light",
-        }}
-      >
         <Navigation>
           <Component {...pageProps} />
         </Navigation>
-      </MantineProvider>
     </div>
   );
 }
