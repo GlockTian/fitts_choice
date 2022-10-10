02bf69fa0dff8645811516d8fbb8c779cb02e8d6
FriedhelmWS <352063465@qq.com>
2022-10-10 18:34:43 +1300

    Add document

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,31 @@
-import { MantineProvider } from '@mantine/core'
-import { Navigation } from '../components/navigation'
-import Head from 'next/head'
-import '../styles/globals.css'
+import { MantineProvider } from "@mantine/core";
+import { Navigation } from "../components/navigation";
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   // create a mantine provider
   return (
     <div>
       <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300;400;500&display=swap" rel="stylesheet"></link>  
+        <link
+          href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
-    <MantineProvider         
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: 'light',
-      }}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
         <Navigation>
           <Component {...pageProps} />
         </Navigation>
-    </MantineProvider>
+      </MantineProvider>
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
